fe688cb8244a4722ad5b7825d46102505144986f
Gareth Andrew <gingerhendrix@hotmail.com>
2008-11-18 12:19:21 +0000

    Added some http functionality

---
+++ src/http.js
@@ -0,0 +1,39 @@
+
+Utils.namespace("Utils.http");
+
+
+Utils.http.createXMLHttpRequest = function(){
+  //Borrowed form Ajax Cookbook - http://ajaxcookbook.org/xmlhttprequest/
+  //Might be better off a fully-fledged wrapper like Sergey Illinsky's
+  
+  if (typeof XMLHttpRequest != "undefined") {
+      return new XMLHttpRequest();
+  } else if (typeof ActiveXObject != "undefined") {
+      return new ActiveXObject("Microsoft.XMLHTTP");
+  } else {
+      throw new Error("XMLHttpRequest not supported");
+  }
+}
+
+
+Utils.http.get = function(uri, callback, errback){
+  var req = Utils.http.createXMLHttpRequest();
+  req.open('GET', uri, true);
+  req.onreadystatechange = function (aEvt) {
+    if (req.readyState == 4) {
+      if(req.status == 200){
+        // Utils.log.info("Loaded " + uri);
+         if(callback && ((typeof callback) == "function")){
+           callback(req);
+        }
+      }else{
+        // Utils.log.info("Error loading " + uri);
+        if(errback && ((typeof errback) == "function")){
+          errback(req);
+        }
+      }
+    }
+  };
+  req.send(null); 
+}
+
