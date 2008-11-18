e6c5b2963498ac4b08844daedbecc6a0adae8e42
Gareth Andrew <gingerhendrix@hotmail.com>
2008-11-18 12:28:44 +0000

    Added basic existence tests for http package

---
+++ test/unit/http_test.js
@@ -0,0 +1,8 @@
+new TestSuite("Http Tests", {
+  testExists : function(t){
+    t.assert(typeof(Utils)=="object",  "Utils not defined");
+    t.assert(typeof(Utils.http)=="object",  "Utils.http not defined");
+    t.assert(typeof(Utils.http.createXMLHttpRequest)== "function",  "Utils.http.createXMLHttpRequest not defined");
+    t.assert(typeof(Utils.http.get) == "function",  "Utils.http.get not defined");
+  }
+});
